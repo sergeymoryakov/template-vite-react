6ccaf95f2f38b31a554b3ec3565aa048cc9ebf65
Sergey Moryakov <sergey@moryakov.com>
2023-11-20 20:25:13 +0200

    add 3D model images

--- src/components/ComingSoon/ComingSoon.jsx
+++ src/components/ComingSoon/ComingSoon.jsx
@@ -2,11 +2,24 @@ import React from "react";
 // import kzLogo from "./assets/kza-logo-t9-h150.svg";
 import LOGO_3D_SAFARI from "../../assets/logo-transparent-hevc-safari.mp4";
 import LOGO_3D_CHROME from "../../assets/logo-transparent-vp9-chrome.webm";
+import AIRCRAFT_3D_SAFARI from "../../assets/aircraft-hevc-safari.mp4";
+import AIRCRAFT_3D_CHROME from "../../assets/aircraft-vp9-chrome.webm";
+import ENGINE_3D_SAFARI from "../../assets/engine-hevc-safari.mp4";
+import ENGINE_3D_CHROME from "../../assets/engine-vp9-chrome.webm";
 import "./ComingSoon.css";
 
 function ComingSoon() {
     return (
         <div className="container">
+            <div>
+                <video className="image-3d" autoPlay loop muted playsInline>
+                    <source
+                        src={AIRCRAFT_3D_SAFARI}
+                        type='video/mp4; codecs="hvc1"'
+                    />
+                    <source src={AIRCRAFT_3D_CHROME} type="video/webm" />
+                </video>
+            </div>
             <div className="horisontal-box">
                 <video className="logo-3d" autoPlay loop muted playsInline>
                     <source
@@ -22,6 +35,15 @@ function ComingSoon() {
                 </h2>
             </div>
             <h3 className="text-gradient">Stay Tuned</h3>
+            <div>
+                <video className="image-3d" autoPlay loop muted playsInline>
+                    <source
+                        src={ENGINE_3D_SAFARI}
+                        type='video/mp4; codecs="hvc1"'
+                    />
+                    <source src={ENGINE_3D_CHROME} type="video/webm" />
+                </video>
+            </div>
         </div>
     );
 }
